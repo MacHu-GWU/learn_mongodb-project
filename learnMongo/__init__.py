b68a958dbfa6787f90a5eda2965b68f399149ff6
MacHu-GWU <husanhe@gmail.com>
2017-02-03 13:08:18 -0500

    2017-02-03
    Delete learnMongo legacy code

--- learnMongo/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__version__ = "0.0.1"
-__short_description__ = "我个人学习MongoDB的笔记"
-__author__ = "Sanhe Hu"
-__license__ = "MIT"
--- learnMongo/cookbook.py/temp.py
+++
@@ -1,6 +0,0 @@
-from learnMongo import test_col
-
-test_col.remove({})
-test_col.insert({"a": 1})
-test_col.update({"a": 1}, {"$set": {"a": 2}})
-print(test_col.find_one({"a": 2}))
